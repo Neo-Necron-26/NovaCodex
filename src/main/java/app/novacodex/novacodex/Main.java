d63acab054fec0086167222c8248b158789d6963
Руслан <ruslanjoraeff@yandex.com>
2025-03-28 10:56:08 +0700

    Add REAMDE.md file

--- src/main/java/app/novacodex/novacodex/Main.java
+++ src/main/java/app/novacodex/novacodex/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Scene scene = new Scene(fxmlLoader.load(), 320, 250);
         stage.setTitle("NovaCodex");
         stage.setScene(scene);
         stage.show();
