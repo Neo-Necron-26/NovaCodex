8508e987b92c4e9ade6edbb0faaaf41997abda75
grbj1 <nikitailinyh88728@gmail.com>
2025-06-01 12:10:09 +0300

    Creating pushable buttons

--- src/main/java/app/novacodex/novacodex/loader/RoleSelectionWindow.java
+++ src/main/java/app/novacodex/novacodex/loader/RoleSelectionWindow.java
@@ -1,4 +1,20 @@
 package app.novacodex.novacodex.loader;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class RoleSelectionWindow {
+
+    @FXML
+    private void handleStudentClick(ActionEvent event) {
+        System.out.println("Выбрана роль: Студент");
+        // Действия при выборе студента (например, загрузка нового окна)
+    }
+
+    @FXML
+    private void handleTeacherClick(ActionEvent event) {
+        System.out.println("Выбрана роль: Преподаватель");
+        // Действия при выборе преподавателя
+    }
 }
